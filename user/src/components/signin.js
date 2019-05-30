bf3582af69305cbc0b785fb77c576bd2ce01a8c2
amirlamri <50244166+amirlamri@users.noreply.github.com>
2019-05-30 02:45:44 +0000

    Update signin
    choix entre type dinscription

--- user/src/components/signin.js
+++ user/src/components/signin.js
@@ -2,6 +2,40 @@ import React, {Component} from 'react';
 import { Link, withRouter } from 'react-router-dom'
 
 export default class Signin extends Component {
+    constructor(props){
+        super(props);
+        this.state = { value: 'client'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+     
+    }
+
+    handleChange(event){
+        this.setState({value : event.target.value})
+    } 
+
+    handleSubmit(event){
+      // alert('selected value: ' + this.state.value);
+        var vl ;
+         if (this.state.value == 'seller'){
+             
+            let path = `/sign_celler`;
+            this.props.history.push(path);
+          
+           
+         }else 
+         {
+            let path = `/sign_client`;
+            this.props.history.push(path);
+         }
+        
+        
+
+    }
+  
+
+
     render(){
         return (
             <section>
@@ -20,10 +54,10 @@ export default class Signin extends Component {
                     <div className="row block-9 " >
                         <div className="col-md-3 order-md-last d-flex"></div>
                         <div className="col-md-6 order-md-last d-flex">
-                            <form action="#" className="bg-white p-5 contact-form">
+                            <form action="#" className="bg-white p-5 contact-form" onSubmit={this.handleSubmit}>
                                 <div class="form-group form-control-lg">
                                     <label for="choix_cnx">sign in as :</label>
-                                    <select class="form-control" id="choix_cnx">
+                                    <select class="form-control" id="choix_cnx" value={this.state.selectValue} onChange={this.handleChange}>
                                     <option id="client">Client</option>
                                     <option id="celler">celler</option>
                                     </select>
